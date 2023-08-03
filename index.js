d5df4e5bafddac718075b065b0b838f627a83fa1
Rizqila1 <ikilatr@gmail.com>
2023-08-03 16:22:09 +0700

    feat: add permisson cors

--- index.js
+++ index.js
@@ -12,30 +12,17 @@ import r_address from "./src/routers/r_address.js";
 
 const app = express();
 
-app.use(cors());
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+    setHeader:
+      "accept-language, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With",
+    credentials: true,
+    optionsSuccessStatus: 204,
+  })
+);
 
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "accept-language, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
